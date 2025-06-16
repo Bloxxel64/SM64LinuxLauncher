680b29a634f633d8d639cc250d011def4773d4cf
Bloxxel64 <70494679+Bloxxel64@users.noreply.github.com>
2025-06-16 14:13:16 -0500

    arch need to actually work you doopid

--- src/backend/install_deps.py
+++ src/backend/install_deps.py
@@ -3,7 +3,7 @@ import subprocess
 aptCmd = ('/usr/bin/apt install -y libsdl2-dev gcc-mips-linux-gnu make build-essential python3-gi python3-gi-cairo '
           'gir1.2-gtk-4.0')
 
-pacmanCmd = ('pacman -S sdl2_gfx sdl2_image sdl2_mixer sdl2_net git gcc-mips-linux-gnu make base-devel python-gobject '
+pacmanCmd = ('pacman -S sdl2_gfx sdl2_image sdl2_mixer sdl2_net git mips64-linux-gnu-gcc make base-devel python-gobject '
              'gtk4')
 
 
--- src/backend/terminal_debug.py
+++ src/backend/terminal_debug.py
@@ -6,18 +6,25 @@ import os
 import install_deps
 import compiler
 
-cmd = input("Please enter a command (e.g. 'help', 'install-depends', etc.): ")
+def main():
+    cmd = input("Please enter a command (e.g. 'help', 'install-depends', etc.): ")
 
-if cmd == "help":
-    print("List of available commands:\n"
-          "help (duh),\n"
-          "install-depends,\n"
-          "clone-repo\n")
+    if cmd == "help":
+        print("List of available commands:\n"
+            "help (duh),\n"
+            "install-depends,\n"
+            "clone-repo\n")
+        main()
 
-if cmd == "install-depends":
-    password = input("\nPlease enter your sudo password, as the following command requires superuser privileges: ")
-    output = install_deps.func_install_deps("apt", password)
-    print(output)
+    if cmd == "install-depends":
+        pkgmgr = input("\nApt or Pacman?: ")
+        password = input("\nPlease enter your sudo password, as the following command requires superuser privileges: ")
+        output = install_deps.func_install_deps(pkgmgr, password)
+        print(output)
+        main()
 
-if cmd == "clone-repo":
-    output = compiler.func_clone_repo("https://github.com/coop-deluxe/sm64coopdx", os.path.expanduser('~') + "/gexcoop")
+    if cmd == "clone-repo":
+        output = compiler.func_clone_repo("https://github.com/coop-deluxe/sm64coopdx", os.path.expanduser('~') + "/gexcoop")
+        main()
+
+main()
